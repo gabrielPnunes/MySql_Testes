0275654f3cafd35784fb64da0ef93e71e7756327
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2024-01-03 21:04:28 -0300

    Add files via upload

---
+++ criando_1_database.sql
@@ -0,0 +1,14 @@
+create database cadastro;
+
+use cadastro;
+
+create table Pessoas(
+nome varchar(30),
+idade tinyint,
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+); 
+
+describe pessoas;
