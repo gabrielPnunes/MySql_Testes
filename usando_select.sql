09792b2e5bfa1eb488a6930e0d5271e07bc1bd5f
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2024-05-06 23:57:52 -0300

    Update usando_select.sql

--- usando_select.sql
+++ usando_select.sql
@@ -7,7 +7,7 @@ order by nome;
 
 select nome,  descricao, carga from cursos
 where ano between 2014 and 2016
-order by nome asc;
+order by nome asc; 
 
 select nome, descricao, carga from cursos
 where ano in (2014,2016,2020)
